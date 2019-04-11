6cfa7b2ae478bcb4d28ad00c6cc21de35be45b1b
Robin <robin@naturewake.com>
2019-04-11 18:53:25 +0800

    initialized an empty Typescript project

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+@sealed
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+function sealed(constructor: Function) {
+    Object.seal(constructor);
+    Object.seal(constructor.prototype);
+    console.log(Object.seal(constructor.prototype), Object.seal(constructor))
+}
+
+// sealed(Greeter);
